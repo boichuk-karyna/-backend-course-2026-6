2fbd07b48de97e89574934615d76f80e68dfee9b
Karina bc2026-6 <Karyna.Boichuk@lnu.edu.ua>
2026-04-27 15:13:58 +0300

    new changes

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const path = require('path');
 const multer = require('multer');
 const http = require('http');
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 program
   .requiredOption('-h, --host <host>', 'адреса сервера')
   .requiredOption('-p, --port <port>', 'порт сервера')
@@ -31,9 +35,61 @@ const upload = multer({ dest: cacheDir });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/RegisterForm.html', (req, res) => res.sendFile(path.join(__dirname, 'RegisterForm.html')));
-app.get('/SearchForm.html', (req, res) => res.sendFile(path.join(__dirname, 'SearchForm.html')));
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Inventory API',
+      version: '1.0.0',
+      description: 'API для управління інвентарем'
+    },
+    servers: [
+      {
+        url: `http://${options.host}:${options.port}`
+      }
+    ]
+  },
+  apis: ['./main.js'] 
+};
 
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+
+app.get('/RegisterForm.html', (req, res) =>
+  res.sendFile(path.join(__dirname, 'RegisterForm.html'))
+);
+
+app.get('/SearchForm.html', (req, res) =>
+  res.sendFile(path.join(__dirname, 'SearchForm.html'))
+);
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Створити інвентар
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - inventory_name
+ *             properties:
+ *               inventory_name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               photo:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       201:
+ *         description: Created
+ */
 app.post('/register', upload.single('photo'), (req, res) => {
   const { inventory_name, description } = req.body;
   if (!inventory_name) {
@@ -53,24 +109,66 @@ app.post('/register', upload.single('photo'), (req, res) => {
 });
 app.all('/register', (req, res) => res.status(405).send('Method not allowed'));
 
+/**
+ * @swagger
+ * /inventory:
+ *   get:
+ *     summary: Отримати всі записи
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 app.get('/inventory', (req, res) => {
   const list = inventoryDB.map(item => ({
     ...item,
-    photoUrl: item.photo ? `http://${options.host}:${options.port}/inventory/${item.id}/photo` : null
+    photoUrl: item.photo
+      ? `http://${options.host}:${options.port}/inventory/${item.id}/photo`
+      : null
   }));
   res.status(200).json(list);
 });
 app.all('/inventory', (req, res) => res.status(405).send('Method not allowed'));
 
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   get:
+ *     summary: Отримати по ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Not found
+ */
 app.get('/inventory/:id', (req, res) => {
   const item = inventoryDB.find(i => i.id === req.params.id);
   if (!item) return res.status(404).send('Not found');
+
   res.status(200).json({
     ...item,
-    photoUrl: item.photo ? `http://${options.host}:${options.port}/inventory/${item.id}/photo` : null
+    photoUrl: item.photo
+      ? `http://${options.host}:${options.port}/inventory/${item.id}/photo`
+      : null
   });
 });
 
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   put:
+ *     summary: Оновити інвентар
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
 app.put('/inventory/:id', (req, res) => {
   const itemIndex = inventoryDB.findIndex(i => i.id === req.params.id);
   if (itemIndex === -1) return res.status(404).send('Not found');
@@ -83,6 +181,19 @@ app.put('/inventory/:id', (req, res) => {
   res.status(200).json(inventoryDB[itemIndex]);
 });
 
+/**
+ * @swagger
+ * /inventory/{id}:
+ *   delete:
+ *     summary: Видалити інвентар
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
 app.delete('/inventory/:id', (req, res) => {
   const itemIndex = inventoryDB.findIndex(i => i.id === req.params.id);
   if (itemIndex === -1) return res.status(404).send('Not found');
@@ -99,6 +210,16 @@ app.delete('/inventory/:id', (req, res) => {
 });
 app.all('/inventory/:id', (req, res) => res.status(405).send('Method not allowed'));
 
+/**
+ * @swagger
+ * /inventory/{id}/photo:
+ *   get:
+ *     summary: Отримати фото
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ */
 app.get('/inventory/:id/photo', (req, res) => {
   const item = inventoryDB.find(i => i.id === req.params.id);
   if (!item || !item.photo) return res.status(404).send('Not found');
@@ -107,26 +228,26 @@ app.get('/inventory/:id/photo', (req, res) => {
   if (!fs.existsSync(photoPath)) return res.status(404).send('Not found');
 
   res.setHeader('Content-Type', 'image/jpeg');
-  res.sendFile(photoPath, { dotfiles: 'allow' });
+  res.sendFile(photoPath);
 });
 
-app.put('/inventory/:id/photo', upload.single('photo'), (req, res) => {
-  const itemIndex = inventoryDB.findIndex(i => i.id === req.params.id);
-  if (itemIndex === -1) return res.status(404).send('Not found');
-  if (!req.file) return res.status(400).send('Bad Request: photo is required');
-
-  const oldPhoto = inventoryDB[itemIndex].photo;
-  if (oldPhoto) {
-    const oldPath = path.join(cacheDir, oldPhoto);
-    if (fs.existsSync(oldPath)) fs.unlinkSync(oldPath);
-  }
-
-  inventoryDB[itemIndex].photo = req.file.filename;
-  saveDB();
-  res.status(200).send('Photo updated');
-});
-app.all('/inventory/:id/photo', (req, res) => res.status(405).send('Method not allowed'));
-
+/**
+ * @swagger
+ * /search:
+ *   post:
+ *     summary: Пошук
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               has_photo:
+ *                 type: string
+ */
 app.post('/search', (req, res) => {
   const { id, has_photo } = req.body;
   const item = inventoryDB.find(i => i.id === id);
@@ -141,8 +262,9 @@ app.post('/search', (req, res) => {
 });
 app.all('/search', (req, res) => res.status(405).send('Method not allowed'));
 
+
 const server = http.createServer(app);
 server.listen(options.port, options.host, () => {
-  console.log(`Server is running at http://${options.host}:${options.port}`);
-  console.log(`Cache directory: ${cacheDir}`);
+  console.log(`Server: http://${options.host}:${options.port}`);
+  console.log(`Swagger: http://${options.host}:${options.port}/api-docs`);
 });
